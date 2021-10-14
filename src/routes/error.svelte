3b74296d0682f3bb8934c9740ed7b241930d0d75
mingyuanyu <mingyuanyu@microsoft.com>
2021-10-14 16:50:32 +0800

    add sapper app

---
+++ src/routes/error.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Error</title>
+</svelte:head>
+
+<h1>Validate error</h1>
+
+<p>Try again</p>
---
+++ src/routes/success.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>Success</title>
+</svelte:head>
+
+<h1>Successfully Validated</h1>
+
+<p>You are good to go with you application. You can close this tab now.</p>
+
+
+<script>
+
+import { onMount } from 'svelte';
+import { stores } from "@sapper/app";
+
+const { page } = stores();
+console.log($page.query);
+
+onMount(() => {
+	setTimeout(() => {
+		window.close();
+	}, 1000);
+});
+
+</script>
